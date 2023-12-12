1fb08d11060db011d0e2b6e3dab7758301dd589d
Arthur <mg00artur@gmail.com>
2023-12-12 14:22:31 +0400

    Fix mitakes

--- aws-vpc-create.sh
+++ aws-vpc-create.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# CONFIG_FILE=~/.aws/config
-# REGION=""
+CONFIG_FILE=~/.aws/config
+REGION=""
 
-# while IFS=' = ' read key value
-# do
-#     if [[ $key == \[*] ]]; then
-#         section=$key
-#     elif [[ $value ]] && [[ $section == '[default]' ]]; then
-#         if [[ $key == 'region' ]]; then
-#             DEFAULT_REGION=$value
-# 	fi    
-#     fi
-# done < $CREDENTIALS_FILE
+while IFS=' = ' read key value
+do
+    if [[ $key == \[*] ]]; then
+        section=$key
+    elif [[ $value ]] && [[ $section == '[default]' ]]; then
+        if [[ $key == 'region' ]]; then
+            DEFAULT_REGION=$value
+	fi    
+    fi
+done < $CREDENTIALS_FILE
 
-# if [[ -z "$DEFAULT_REGION" ]]; then
-# 	REGION=$REGION
-# else
-# 	REGION=$DEFAULT_REGION
-# fi
+if [[ -z "$DEFAULT_REGION" ]]; then
+	REGION=$REGION
+else
+	REGION=$DEFAULT_REGION
+fi
 
 #Create VPC with tags
 echo "Creating VPC...."
@@ -70,7 +70,7 @@ echo "Creating Route Table...."
 CREATED_ROUTE_TABLE_ID=$(aws ec2 create-route-table --vpc-id $CREATED_VPC_ID --query RouteTable.RouteTableId --output text)
 ROUTE_TABLE_TAGS=$(aws ec2 create-tags --resources $CREATED_ROUTE_TABLE_ID --tags Key=Name,Value=RouteTable-$CREATED_VPC_ID)
 CREATED_ROUTE=$(aws ec2 create-route --route-table-id $CREATED_ROUTE_TABLE_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $CREATED_INTERNET_GATEWAY_ID)
-$(aws ec2 associate-route-table --route-table-id $CREATED_ROUTE_TABLE_ID --subnet-id $CREATED_SUBNET_ID)
+ASSOCIATE_ROUTE=$(aws ec2 associate-route-table --route-table-id $CREATED_ROUTE_TABLE_ID --subnet-id $CREATED_SUBNET_ID --output text)
 
 if [ $? -eq 0 ]; then
 	echo "Route Table is successfully created"
@@ -90,7 +90,8 @@ $(aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --proto
 if [ $? -eq 0 ]; then
 	echo "The Vpc is successfully  created"
 else
-	echo "Failed creating $(aws ec2 delete-vpc --vpc-id $CREATED_VPC_ID)"
+	echo "Failed creating"
+	$(aws ec2 delete-vpc --vpc-id $CREATED_VPC_ID)
 	exit 1
 fi
 
@@ -100,5 +101,5 @@ fi
 
 echo "Creating EC2 Instance"
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name lesson-3-virginia --security-group-ids $SECURITY_GROUP_ID --subnet-id $CREATED_SUBNET_ID --network-interfaces '[{"DeviceIndex":0,"AssociatePublicIpAddress":true}]' --query Instances.InstanceId --output text)
-INSTANCE_TAGS=$(aws crate-tags --resources $INSTANCE_ID --tags Key=Name,Value=EC2-VPCid-$CREATED_VPC_ID)
+INSTANCE_TAGS=$(aws create-tags --resources $INSTANCE_ID --tags Key=Name,Value=EC2-VPCid-$CREATED_VPC_ID)
 echo "Done"
